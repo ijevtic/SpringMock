b7b641aa0661dabda4c97ea407f1c8da613946fd
Ivan Jevtic <ivanjevtic501@gmail.com>
2023-11-09 01:11:21 +0100

    Add custom Pair class

---
+++ src/com/raf/util/Pair.java
@@ -0,0 +1,27 @@
+package com.raf.util;
+
+public class Pair<T,V> {
+    private T key;
+    private V value;
+
+    public Pair(T key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public T getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setKey(T key) {
+        this.key = key;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}
